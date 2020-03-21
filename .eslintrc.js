26fd3becfe77e9aacad2df82fc07b914ddeeecdc
Daniel Ma <dma@ministrycentered.com>
2020-03-21 13:57:03 -0700

    ok we're back

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        trailingComma: 'es5',
+        semi: true,
+        jsxSingleQuote: true,
+        singleQuote: true,
+        useTabs: false,
+      },
+    ],
+  },
+}
